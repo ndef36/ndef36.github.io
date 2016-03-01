a8128457da2eccd57e9d0037c9123ab89656a9ce
ndef36 <kgx10@hotmail.com>
2016-02-29 23:26:10 -0500

    a

--- views/js/main.js
+++ views/js/main.js
@@ -433,11 +433,11 @@ var resizePizzas = function(size) {
     function sizeSwitcher (size) {
       switch(size) {
         case "1":
-          return 25;
+          return 25%;
         case "2":
-          return 33.33;
+          return 33.33%;
         case "3":
-          return 50;
+          return 50%;
         default:
           console.log("bug in sizeSwitcher");
       }
